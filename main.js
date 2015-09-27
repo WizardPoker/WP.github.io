629b66e370666310a661e4a95acc07dbd8e1bebb
WizardPoker <btb2194@gmail.com>
2015-09-27 02:43:40 -0400

    css + js

---
+++ main.js
@@ -0,0 +1,40 @@
+
+function registerNavButtons() {
+  var sections = ['about', 'writing', 'contact'];
+
+  sections.forEach(function(section) {
+    clickAndScroll('#nav-' + section, '#' + section + '-section');
+  });
+}
+
+function clickAndScroll(link, section) {
+  var delay = 700;
+
+  $(link).click(function() {
+    $('html, body').animate({
+      scrollTop: $(section).offset().top
+    }, delay);
+
+    return false;
+  });
+}
+
+function handleContactSubmission() {
+  $('form').submit(function(e) {
+    e.preventDefault();
+    var first = $('input[name=first]').val();
+    var last = $('input[name=last]').val();
+    var subject = $('input[name=subject]').val();
+    var message = $('textarea[name=message]').val();
+
+    var url = 'mailto:youremail@gmail.com?&subject=' + first + ' ' + last + ': ' + subject + '&body=' + message;
+
+    var win = window.open(url, '_blank');
+    win.focus();
+  });
+}
+
+$(function() {
+  registerNavButtons();
+  handleContactSubmission();
+});
